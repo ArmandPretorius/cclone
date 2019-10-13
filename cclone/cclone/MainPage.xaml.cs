3ba00aee006ff4a65c692dd1a4eeaad628c75134
Armand Pretorius <armand@jackstudios.co.za>
2019-10-13 17:17:06 +0200

    feat: Successful pull from AI API
    Result of Look-a-like name received.

--- cclone/cclone/MainPage.xaml.cs
+++ cclone/cclone/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Clarifai.API;
+using Clarifai.DTOs.Inputs;
+using Clarifai.DTOs.Predictions;
 
 namespace cclone
 {
@@ -16,6 +19,33 @@ namespace cclone
         public MainPage()
         {
             InitializeComponent();
+            Main();
+        }
+
+        public async void Main()
+        {
+
+            var image = "https://samples.clarifai.com/celebrity.jpeg";
+
+            var client = new ClarifaiClient("64a64aff0198437781affe9a9ea31803");
+
+
+            var res = await client.PublicModels.CelebrityModel
+                 .Predict(new ClarifaiURLImage(image))
+               .ExecuteAsync();
+
+            // Print the concepts
+            foreach (var faceConcepts in res.Get().Data)
+            {
+                Console.WriteLine($"{faceConcepts.Concepts[0].Name}");
+                // celebName = faceConcepts.Concepts[0].Name;
+                titleResult.Text = faceConcepts.Concepts[0].Name;
+                //foreach(var concept in faceConcepts.Concepts)
+                //{
+                //    Console.WriteLine($"{concept.Name}:{concept.Value}");
+                //}
+
+            }
         }
     }
 }
